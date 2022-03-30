ea1163578c06b531ce4be6f0f00bee6a725ae38c
Vex0on <73704968+Vex0on@users.noreply.github.com>
2022-03-30 02:52:44 +0200

    Delete lab11 directory

--- lab11/src/pl/imiajd/Sosnowski/PairUtil.java
+++
@@ -1,9 +0,0 @@
-package pl.imiajd.Sosnowski;
-
-public class PairUtil<T> {
-    public static <T> Pair<T> swap(Pair<T> duo){
-        Pair<T> last_pair = new Pair<>(duo.getFirst(), duo.getSecond());
-        duo.swap();
-        return last_pair;
-    }
-}
