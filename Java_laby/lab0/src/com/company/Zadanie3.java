509c8ddcab42e63d6bd837e45a473769f30de21c
Jacek Sosnowski <73704968+Vex0on@users.noreply.github.com>
2025-11-27 00:27:57 +0100

    Delete Java_laby directory

--- Java_laby/lab0/src/com/company/Zadanie3.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Zadanie3 {
-
-    public static void main(String[] args) {
-        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
-    }
-}
--- Java_laby/lab06/src/com/company/TEST_IntegerSet.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class TEST_IntegerSet {
-    public static void main(String[] args) {
-
-    }
-}
